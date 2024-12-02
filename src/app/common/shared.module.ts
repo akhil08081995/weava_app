9132dd163f3dd77d12879fc486f44c170791761b
surajpaul2412 <suraj.paul.69@gmail.com>
2024-12-02 21:50:04 +0530

    dashboard

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -4,16 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent],
+  declarations: [HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
   ],
-  exports: [HeaderComponent, SidebarComponent], // Export components for use in other modules
+  exports: [HeaderComponent, SidebarComponent, FooterComponent], // Export components for use in other modules
   providers: [],
 })
 export class SharedModule {} // Renamed module
